03d98b031456448f5d7057175ccd76c7cb480eac
Akash <akashlgowda203@gmail.com>
2024-06-29 00:16:25 +0530

    updated carousal

--- src/components/Carousal.js
+++ src/components/Carousal.js
@@ -1,17 +1,19 @@
 import React from 'react'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 export default function Carousal() {
   return (
     <div><div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
     <div className="carousel-inner">
       <div className="carousel-item active">
-        <img src="https://source.unsplash.com/random/900×700/?burger" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..."/>
+        <img src="mernapp\resources\Carousal imgs\burger.jpeg" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..."/>
       </div>
       <div className="carousel-item">
-        <img src="https://source.unsplash.com/random/900×700/?pastry" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..."/>
+        <img src="mernapp\resources\Carousal imgs\pastry.jpeg" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..."/>
       </div>
       <div className="carousel-item">
-        <img src="https://source.unsplash.com/random/900×700/?barbeque" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..."/>
+        <img src="mernapp\resources\Carousal imgs\barbeque.jpeg" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..."/>
       </div>
     </div>
     <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
