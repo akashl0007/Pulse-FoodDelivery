69ffc55a1f2d2a44172b74e6a16327ee8476cae3
Akash <akashlgowda203@gmail.com>
2024-08-09 20:08:23 +0530

    implemented mongoose to read data from MongoDB

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://pulse:pulse@cluster0.frxfcpj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoURI = 'mongodb+srv://pulse:pulse@cluster0.frxfcpj.mongodb.net/Pulse-MERN?retryWrites=true&w=majority&appName=Cluster0';
 
 const mongoDB = async () => {
   try {
     await mongoose.connect(mongoURI, {
-      useNewUrlParser: true, // Corrected option
-      useUnifiedTopology: true, // Recommended option
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log("Connected to MongoDB");
+
+    const fetched_data = await mongoose.connection.db.collection("food_items").find({}).toArray();
+    // console.log(fetched_data);
+
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
