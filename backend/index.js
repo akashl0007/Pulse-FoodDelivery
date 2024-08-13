2f19ec79077f99f7cf8ddfa3b945d3c25b19682d
Akash <akashlgowda203@gmail.com>
2024-08-13 16:34:35 +0530

    implemented Routes

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,13 @@ const port = 5000;
 const mongoDB = require("./db");
 mongoDB();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', (req, res) => {  //app.get is an endpoint. GET route to retrive data from the database.
+  res.send('Hello World!------');
 });
 
+app.use(express.json())
+const createUserRoutes = require('./Routes/CreateUser');
+app.use('/api', createUserRoutes);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
