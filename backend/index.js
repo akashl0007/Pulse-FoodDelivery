8ee8dd167b693424c0db049a4b933a2cc1e3b06b
Akash <akashlgowda203@gmail.com>
2024-08-20 18:08:36 +0530

    implemented Login and Signup pages

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,25 @@ const port = 5000;
 const mongoDB = require("./db");
 mongoDB();
 
-app.get('/', (req, res) => {  //app.get is an endpoint. GET route to retrive data from the database.
+// Middleware to handle CORS . ==> When frontend and backend are running on different ports (here 3000 and 5000)
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+        "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+app.use(express.json());
+
+// Route for creating a user
+const createUserRoutes = require('./Routes/CreateUser');
+app.use('/api', createUserRoutes);
+
+app.get('/', (req, res) => {
   res.send('Hello World!------');
 });
 
-app.use(express.json())
-const createUserRoutes = require('./Routes/CreateUser');
-app.use('/api', createUserRoutes);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+let navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(
+      JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      })
+    );
+
+    const response = await fetch("http://localhost:5000/api/loginUser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
+    const json = await response.json();
+    console.log(json);
+
+    if (!json.success) {
+      alert("Enter valid credentials.");
+    }
+    if (json.success) {
+      navigate("/");
+    }
+  };
   return (
-    <div>Login</div>
-  )
+    <>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              value={credentials.email}
+              onChange={handleChange}
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              value={credentials.password}
+              onChange={handleChange}
+              id="exampleInputPassword1"
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+          <Link to="/CreateUser" className="m-3 btn btn-danger">
+            New User
+          </Link>
+        </form>
+      </div>
+    </>
+  );
 }
