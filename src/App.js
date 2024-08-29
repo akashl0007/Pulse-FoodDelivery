54e2cbb723b009830d69393e7d7e0731af7b93a4
Akash <akashlgowda203@gmail.com>
2024-08-29 23:45:47 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
 import './App.css';
 import Home from './screens/Home';
 import {
   BrowserRouter as Router,
   Routes,
   Route
-} from "react-router-dom";
+} from 'react-router-dom';
 import Login from './screens/Login';
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
@@ -18,17 +17,16 @@ function App() {
   return (
     <CartProvider>
       <Router>
-      <div>
-        <Routes>
-          <Route exact path= "/" element={<Home/>} />
-          <Route exact path= "/login" element={<Login/>} />
-          <Route exact path= "/createuser" element={<Signup/>} />
-          <Route exact path= "/myOrder" element={<MyOrder/>} />
-        </Routes>
-      </div>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/createuser" element={<Signup />} />
+            <Route path="/myOrder" element={<MyOrder />} />
+          </Routes>
+        </div>
       </Router>
     </CartProvider>
-    
   );
 }
 
